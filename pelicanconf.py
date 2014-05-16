597c1cd6b8e759d6eaa509492dd7715ab0092a3a
Elana Hashman <elana@hashman.ca>
2014-05-16 00:57:55 -0400

    Unbreak dev settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,9 +4,9 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Elana Hashman'
 SITENAME = u'Google Summer of Code 2014 Blog'
-SITEURL = '/home/elana/pelican2/output'
+SITEURL = '/home/elana/pelican/output'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'US/Eastern'
 
 THEME = 'mnmlist'
 
@@ -16,16 +16,6 @@ DEFAULT_LANG = u'en'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
 
